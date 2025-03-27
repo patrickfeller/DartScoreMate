830608835f6981357d32a58af643069980ef36ad
Patrick Feller <patrick.feller@hotmail.at>
2025-03-27 16:50:32 +0100

    CHORE: removed all unnecessary functions

--- src/python/camera_handling.py
+++ src/python/camera_handling.py
@@ -23,9 +23,9 @@ class camera:
             "10 FPS": 0.1,
             "5 FPS": 0.2
         }
-
+ 
     def get_available_cameras(self):
-        """Get a list of available camera indices."""
+        """Get a list of available camera indices with name."""
         if not self.available_cameras:
             for i in range(6):  # Check first 6 indices
                 cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)  
@@ -33,31 +33,3 @@ class camera:
                     self.available_cameras.append(i)
                     cap.release()
         return self.available_cameras
-
-    def release_camera(self, camera_name):
-        """Safely release a camera instance."""
-        if camera_name in self.camera_instances:
-            cap = self.camera_instances[camera_name]
-            if cap is not None:
-                cap.release()
-            del self.camera_instances[camera_name]
-            if camera_name in self.current_frames:
-                del self.current_frames[camera_name]
-            if camera_name in self.last_update:
-                del self.last_update[camera_name]
-            if camera_name in self.error_counts:
-                del self
-    
-    def update_camera_frame(self, camera_name):
-        """Update the frame for the specified camera."""
-        cap = self.camera_instances[camera_name]
-        ret, frame = cap.read()
-        if ret:
-            self.current_frames[camera_name] = frame
-            self.last_update[camera_name] = time.time()
-        else:
-            if camera_name in self.error_counts:
-                self.error_counts[camera_name] += 1
-            else:
-                self.error_counts[camera_name] = 1
-            self.release_camera(camera_name)
