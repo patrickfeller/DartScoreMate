a50be70c7e005b41078eb310257f74b879adc71b
Kathrin Lindauer <klindauer@aol.com>
2025-03-31 16:59:01 +0200

    FEAT: created simple sql script for game table

---
+++ sql/game_table.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE `game` (
+	`game_mode` SMALLINT NOT NULL AUTO_INCREMENT UNIQUE,
+	`player_A` VARCHAR(63),
+	`player_B` VARCHAR(63),
+	`score_player_A` SMALLINT,
+	`score_player_B` SMALLINT,
+	`game_id` SMALLINT AUTO_INCREMENT UNIQUE,
+	PRIMARY KEY(`game_id`)
+);
+
