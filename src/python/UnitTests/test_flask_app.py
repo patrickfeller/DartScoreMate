523884ecbd74f97bf3672043d9e8287341c5ee9f
Daniel Hillebrand <2410837979@stud.fh-kufstein.ac.at>
2025-04-04 13:32:00 +0200

    FEAT: added examplatory selenium test

--- src/python/UnitTests/test_flask_app.py
+++ src/python/UnitTests/test_flask_app.py
@@ -1,6 +1,12 @@
 import unittest
 from unittest.mock import patch
 from src.flask_app.main import app
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+import os
+import subprocess
 
 # in this script, we can add tests for main.py.
 # At the moment, I only created one to work on Gitlab Integration.
@@ -39,6 +45,50 @@ class FlaskUnitTest(unittest.TestCase):
         # self.assertIn(b"45", response.data)
         # self.assertIn(b"60", response.data)
 
+class IntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        os.environ['FLASK_APP'] = 'src.flask_app.main' 
+        # initialize the Flask test client
+        self.flask_process = subprocess.Popen(['flask', 'run', '--port', '5000'])
+        self.browser =  webdriver.Chrome()
+        self.browser.implicitly_wait(20)
+
+    def tearDown(self):
+        self.browser.quit()
+        self.flask_process.terminate()
+
+
+    def test_game(self):
+        self.browser.get("http://localhost:5000/play")
+        time.sleep(3)
+        self.browser.implicitly_wait(2)
+        player1_input = self.browser.find_element(By.ID, "player1name")
+        player2_input = self.browser.find_element(By.ID, "player2name")
+        
+        # fill in player names
+        player1_input.send_keys("John")
+        player2_input.send_keys("Phil")
+
+        # press submit button
+
+        submit_button = self.browser.find_element(By.CSS_SELECTOR, "input[type='submit'][value='Play']")
+        submit_button.click()
+        time.sleep(3)
+        
+        
+        # Check if the Browser title updated to "Dart App, New Game"
+        self.assertEqual(self.browser.title, 'Dart App: New Game')
+
+        # check if the expected route was used
+        self.assertEqual( self.browser.current_url, "http://localhost:5000/game/John/Phil/501/1")
+
+        player1_score = self.browser.find_element(By.ID, "playerA_score").text
+        player2_score = self.browser.find_element(By.ID, "playerB_score").text
+
+        self.assertTrue(player1_score=="501"), "Expect player A to start with score 501"
+        self.assertTrue(player2_score=="501"), "Expected player B to start with score 501"
+    
 
 if __name__ == "__main__":
     unittest.main()
