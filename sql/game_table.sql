08c47e135128d13cfaba67278aae10c4645f2e7f
Kathrin Lindauer <klindauer@aol.com>
2025-04-09 20:17:52 +0200

    FIX: small errror in sql-script: only one auto-increment allowed

--- sql/game_table.sql
+++ sql/game_table.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TABLE `game` (
-	`game_mode` SMALLINT NOT NULL AUTO_INCREMENT UNIQUE,
+	`game_mode` SMALLINT NOT NULL,
 	`player_A` VARCHAR(63),
 	`player_B` VARCHAR(63),
 	`score_player_A` SMALLINT,
